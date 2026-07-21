5788604e2ab2ec22f4b0dff55fdd3a263abf295b
razor <ramseysimeon007@gmail.com>
2026-07-21 02:07:51 +0000

    Enable scheduling in NotificationServiceApplication

--- backend/notification-service/src/main/java/com/syntra/notification/NotificationServiceApplication.java
+++ backend/notification-service/src/main/java/com/syntra/notification/NotificationServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableScheduling    
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
